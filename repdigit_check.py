0546c389dd9cbe6c4fd7ad85ae3b6edd657feadf
hibigeek <hibi.miyamoto@gmail.com>
2016-03-25 15:49:58 +0900

    has_repdigit added

--- repdigit_check.py
+++ repdigit_check.py
@@ -1,7 +1,18 @@
 # coding: utf-8
 #ゾロ目判定 numberには任意の整数
 def is_repdigit(number):
-    if all(y == str(number)[0] for y in str(number)[1:]):
+  if all(y == str(number)[0] for y in str(number)[1:]):
+      return True
+  else:
+      return False
+
+def has_repdigit(number):
+    repdigits = ["00","11","22","33","44","55","66","77","88","99"]
+
+    for repdigit in repdigits:
+      if repdigit in str(number):
         return True
-    else:
-        return False
+  
+    return False
+
+has_repdigit(111111111111111)
