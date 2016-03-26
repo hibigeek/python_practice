2f684a5b91227795d71bc6339eee737edcfb5048
hibigeek <hibi.miyamoto@gmail.com>
2016-03-26 18:29:27 +0900

    hit_and_blow

--- hit_and_blow.py
+++ hit_and_blow.py
@@ -11,6 +11,44 @@
 なおルール上4422などのゾロ目の正解は出ないようにしましょう。
 また、先頭が0だとn桁の数字じゃなくなるのでせっかくなので0は発生させないようにしましょう。
 """
+import random
 
-from random import randint
+print "Hit & Blow"
+a = []
+digit = 0
+while digit < 1:
+  digit = int(raw_input("Choice play digit:  "))
 
+r = random.randint(1, 9)
+while len(a) != digit:
+	 if r not in a:
+		 a.append(r)
+	 r = random.randint(1, 9)
+
+
+guess = []
+s = 0
+while s != digit:
+	 while len(guess) != digit:
+		 x = int(raw_input("Input Your guess one by one:"))
+		 if x not in guess:
+			 guess.append(x)
+			 print guess
+		 else:
+			 print "Can't input same number"
+
+   
+   #リストを集合型にして論理積をとる。そうすると、リストの中の重複したものがreturnされる。ただし順不同
+   #a =[1,2,3,4] & b = [4,3,2,0] -> c [2,3,4]
+	 c = set(a)
+	 d = set(guess)
+	 c &= d
+	 s = 0
+
+	 for i in range(digit):
+		 if a[i] == guess[i]:
+			 s += 1
+	 print str(len(c) - s) + "Blow"
+	 print str(s) + "Hit"
+	 guess = []
+print "You Win!"
